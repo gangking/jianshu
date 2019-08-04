1f060aaa667d682a0ecc330c2aab83eca7bf0179
gangking <gangking@foxmail.com>
2019-08-05 00:14:52 +0800

    deploy

---
+++ precache-manifest.d96d69d4f6efadf34d2d5d9b1db9afdb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebecdfd37a5f4b92af3e124df90dd1c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46466aa5289de630a4c2",
+    "url": "/static/js/2.c9b46c17.chunk.js"
+  },
+  {
+    "revision": "81d1ddaee73743f9631b",
+    "url": "/static/js/main.41b7a738.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
